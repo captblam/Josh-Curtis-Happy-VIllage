0bf50cb6f52bbaeee68ab3a8d1da7f87f3f4b3b8
captblam <44243846+captblam@users.noreply.github.com>
2018-12-10 15:56:26 -0500

    Miner and farmer

---
+++ Assets/Scripts/Curt's Scripts/HoeScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoeScript : MonoBehaviour {
+    public GameObject Hoe;
+    bool keyHit = false;
+    float currLerpTime = 0, LerpTime = 5, perc;
+   [SerializeField] Vector3 currentPos, endPos, position;
+    private void Start()
+    {
+        position = transform.position;
+        currentPos = transform.position;
+    }
+    // Update is called once per frame
+    void Update () {
+        currentPos = transform.position;
+        if (Input.GetButtonDown("Attack"))
+        {
+            keyHit = true;
+        }
+        if (Input.GetButtonUp("Attack"))
+        {
+            keyHit = false;
+        }
+        if (keyHit == true)
+        {
+            currLerpTime += (15 * Time.deltaTime);
+            if (currLerpTime >= LerpTime)
+            {
+                currLerpTime = LerpTime;
+            }
+            perc = currLerpTime / LerpTime;
+            Hoe.transform.position = Vector3.Lerp(currentPos, endPos, perc);
+        }
+    }
+}

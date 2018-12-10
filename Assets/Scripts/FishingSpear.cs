3cad1cb135ffe4611bd98fe29ef4ce8a001073a9
Snipin74 <josh.cove74@gmail.com>
2018-12-10 15:00:48 -0500

    made some prefabs

--- Assets/Scripts/FishingSpear.cs
+++ Assets/Scripts/FishingSpear.cs
@@ -23,11 +23,11 @@ public class FishingSpear : MonoBehaviour {
     // Update is called once per frame
     void Update () {
 
-        goBack = spearSpawn.transform.position;
-        if (keyHit == false)
-        {
-            transform.position = goBack;
-        }
+        //goBack = spearSpawn.transform.position;
+        //if (keyHit == false)
+        //{
+        //    transform.position = goBack;
+        //}
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             transform.Rotate(120, 0, 0);
@@ -38,39 +38,39 @@ public class FishingSpear : MonoBehaviour {
             transform.Rotate(-120, 0, 0);
         }
 
-        if (Input.GetKeyDown(KeyCode.F) && gameManager.instance.getControlled() == gameObject)
-        {
-            keyHit = true;
-            player.GetComponent<RPGCharacterController>().canWalk = false;
-            currentPos = Spear.transform.position;
-            endPos = Spear.transform.position + Vector3.forward * distance;
-            goBack = Spear.transform.position + Vector3.forward / distance;
-            player.GetComponent<RPGCharacterController>().runSpeed = 0;
-            player.GetComponent<RPGCharacterController>().walkSpeed = 0;
-        }
+        //if (Input.GetKeyDown(KeyCode.F) && gameManager.instance.getControlled() == gameObject)
+        //{
+        //    keyHit = true;
+        //    player.GetComponent<RPGCharacterController>().canWalk = false;
+        //    currentPos = Spear.transform.position;
+        //    endPos = Spear.transform.position + Vector3.forward * distance;
+        //    goBack = Spear.transform.position + Vector3.forward / distance;
+        //    player.GetComponent<RPGCharacterController>().runSpeed = 0;
+        //    player.GetComponent<RPGCharacterController>().walkSpeed = 0;
+        //}
 
-        if (Input.GetKeyUp(KeyCode.F))
-        {
-            //transform.position = spearSpawn.transform.position;
-            keyHit = false;
-            player.GetComponent<RPGCharacterController>().canWalk = true;
-            player.GetComponent<RPGCharacterController>().runSpeed = 15;
-            player.GetComponent<RPGCharacterController>().walkSpeed = 3;
-            LerpTime = 5;
-            currLerpTime = 0;
+        //if (Input.GetKeyUp(KeyCode.F))
+        //{
+        //    //transform.position = spearSpawn.transform.position;
+        //    keyHit = false;
+        //    player.GetComponent<RPGCharacterController>().canWalk = true;
+        //    player.GetComponent<RPGCharacterController>().runSpeed = 15;
+        //    player.GetComponent<RPGCharacterController>().walkSpeed = 3;
+        //    LerpTime = 5;
+        //    currLerpTime = 0;
 
-        }
+        //}
 
-        if (keyHit == true)
-        {
-            currLerpTime += (15*Time.deltaTime);
-            if (currLerpTime >= LerpTime)
-            {
-                currLerpTime = LerpTime;
-            }
-            perc = currLerpTime / LerpTime;
-            Spear.transform.position = Vector3.Lerp(currentPos, endPos, perc);
-        }
+        //if (keyHit == true)
+        //{
+        //    currLerpTime += (15*Time.deltaTime);
+        //    if (currLerpTime >= LerpTime)
+        //    {
+        //        currLerpTime = LerpTime;
+        //    }
+        //    perc = currLerpTime / LerpTime;
+        //    Spear.transform.position = Vector3.Lerp(currentPos, endPos, perc);
+        //}
         
     }
 

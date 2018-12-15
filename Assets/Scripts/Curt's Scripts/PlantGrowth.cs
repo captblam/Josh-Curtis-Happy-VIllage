d798cc5ef21ab62fbe613151fd8fe9141198348a
captblam <44243846+captblam@users.noreply.github.com>
2018-12-15 14:35:12 -0500

    started plant growth

---
+++ Assets/Scripts/Curt's Scripts/PlantGrowth.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantGrowth : MonoBehaviour {
+    [SerializeField]float growthTimer, timer, maxHeight, height, expandTimer;
+    
+	// Use this for initialization
+	void Start () {
+        growthTimer = timer;
+        expandTimer = timer * 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        growthTimer -= Time.deltaTime;
+        expandTimer -= Time.deltaTime;
+        Grow();
+	}
+    void Grow()
+    {
+        if(growthTimer <= 0 && height != maxHeight)
+        {
+            transform.localScale += new Vector3(0, .1f, 0);
+            height++;
+            growthTimer = timer;
+        }
+        if(expandTimer <= 0 && height != maxHeight)
+        {
+            transform.localScale += new Vector3(.3f, 0, .3f);
+            expandTimer = timer * 2;
+           
+        }
+    }
+}

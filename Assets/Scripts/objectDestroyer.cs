39c1a05da24651cdbc1d31b924f2d929352f7b2b
Snipin74 <josh.cove74@gmail.com>
2018-12-19 14:28:56 -0500

    did some stuff and some things and some more stuff
    and things

---
+++ Assets/Scripts/objectDestroyer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectDestroyer : MonoBehaviour {
+
+    void DestroyObject()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Destroy"))
+        {
+            DestroyObject();
+        }
+    }
+}

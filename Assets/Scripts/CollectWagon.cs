9699cbfddb2285c9e9550c9546333b129d3ef121
captblam <44243846+captblam@users.noreply.github.com>
2019-01-07 18:22:45 -0500

    Horse Test 3

--- Assets/Scripts/CollectWagon.cs
+++ Assets/Scripts/CollectWagon.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class CollectWagon : MonoBehaviour {
 
     private ScoreManager scoreHandler;
+    private int RockNum = 0;
+    [SerializeField]private List<GameObject> Rocks = new List<GameObject>();
 
 	// Use this for initialization
 	void Start () {
         scoreHandler = FindObjectOfType<ScoreManager>();
+        
     }
 
     private void OnTriggerEnter(Collider other)
@@ -17,8 +20,32 @@ public class CollectWagon : MonoBehaviour {
         {
             scoreHandler.GiveStone();
             Destroy(other.gameObject);
-
+            RockNum++;
+            
         }  
+    }
+    void Rck()
+    {
+        if (RockNum >= 3)
+        {
+
+        }
+        else if (RockNum >= 5)
+        {
+
+        }
+        else if (RockNum >= 7)
+        {
+
+        }
+        else if (RockNum >= 10)
+        {
+
+        }
+        else
+        {
+
+        }
     }
 }
 
